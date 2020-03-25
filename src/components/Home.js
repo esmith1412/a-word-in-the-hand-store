499e3cf4307d93f2d3a61348e2dee0247f65b015
Elijah Smith <esmith1412@posteo.net>
2020-03-25 12:44:23 -0400

    Added jumbotron and background image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import background_img from '../book-stack-2794002_1920.jpg'
 
 
 export default function Home() {
   return (
-    <article>This is the home page</article>
+    <article class="jumbotron-container jumbotron">
+      <img src={background_img} alt="Spanish book" />
+      <section>
+        <h1 class="display-4">Welcome to A Word in the Hand!</h1>
+        <p class="lead">This is your home for affordable and accurate Spanish to English translations.</p>
+        <hr class="my-hr my-4" />
+        <p>If you wish to learn more about our company, please click the button below:</p>
+        <Link to="/about">
+          <button class="btn btn-lg bg-danger text-warning">Learn More</button>
+        </Link>
+      </section>
+    </article>
   )
 }
