6b7cc7aa76b8ef014e10a75ac02a6c720b7e7626
Elijah Smith <esmith1412@posteo.net>
2020-03-25 21:31:32 -0400

    Added 404 error message to Default component

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 
-export default function Default() {
-  return (
-    <article>This is the default page</article>
-  )
+export default class Default extends React.Component {
+  render() {
+
+    return (
+      <article className="container">
+        <section className="row">
+          <section className="col-10 mx-auto text-center text-title pt-5">
+            <h1 className="display-3">404 Error</h1>
+            <h2>
+              The following page was not found:{" "}
+              <span>&quot;{this.props.location.pathname}&quot;.</span>
+            </h2>
+          </section>
+        </section>
+      </article>
+    )
+  }
 }
